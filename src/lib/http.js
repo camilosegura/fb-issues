0b9bd4b6db845e5026c25ab98e91f19197741312
Camilo Segura <camilosegurar@gmail.com>
2021-04-24 19:08:20 -0500

    Add axios instance

---
+++ src/lib/http.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const { CancelToken } = axios;
+const GITHUB_HOST = 'https://api.github.com';
+
+export default class Http {
+  constructor({ host = GITHUB_HOST, ...config } ) {
+    this.httpInstance = axios.create({
+      baseURL: host,
+      ...config,
+    });
+  }
+
+  callApi({ url, method, data = {}, params = {} }) {
+    const source = CancelToken.source();
+
+    const request = this.httpInstance.request({
+      url,
+      method,
+      data,
+      params,
+      cancelToken: source.token
+    });
+
+    request.cancel = () => {
+      source.cancel('Operation canceled by the user.');
+    }
+
+    return request;
+  }
+
+  get({ url, params = {} }) {
+    return this.callApi({ url, method: 'GET', params });
+  }
+
+  post({ url, data, params = {} }) {
+    return this.callApi({ url, method: 'POST', data, params });
+  }
+
+  put({ url, data, params = {} }) {
+    return this.callApi({ url, method: 'PUT', data, params });
+  }
+
+  delete({ url }) {
+    return this.callApi({ url, method: 'DELETE' });
+  }
+
+  setRequestInterceptor(callBack) {
+    this.httpInstance.interceptors.request.use(callBack, (error) => Promise.reject(error))
+  }
+}
